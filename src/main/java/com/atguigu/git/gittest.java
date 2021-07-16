b9f891567e9feff5baffd2844d19491410a692dc
Dfei <d172839ing880@126.com>
2021-07-16 10:30:03 +0800

    master second commit

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -5,7 +5,7 @@ public class gittest {
         System.out.println("hello git!");
         System.out.println("hello git!2");
         System.out.println("hello git!3");
-        System.out.println("hello git!4");
+        System.out.println("master test");
 
     }
 }
